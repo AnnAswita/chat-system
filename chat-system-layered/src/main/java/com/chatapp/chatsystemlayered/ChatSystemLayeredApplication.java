d59f83564ba272641ccb8717808288678656fb41
Ann Aswita <25250728@studentmail.ul.ie>
2026-07-24 20:09:18 +0100

    Initial Layered application files

---
+++ chat-system-layered/src/main/java/com/chatapp/chatsystemlayered/ChatSystemLayeredApplication.java
@@ -0,0 +1,13 @@
+package com.chatapp.chatsystemlayered;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatSystemLayeredApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatSystemLayeredApplication.class, args);
+    }
+
+}
